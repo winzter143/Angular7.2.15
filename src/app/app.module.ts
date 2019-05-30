c70dea44f6b6ccc8f32b1f3e7934c4032ab7e796
syntax3rror <winzter@gmail.com>
2019-05-30 10:35:54 +0800

    Adding new service InMemory   - ng generate service InMemoryData    modified:   package-lock.json    modified:   src/app/app.module.ts    new file:   src/app/in-memory-data.service.spec.ts    new file:   src/app/in-memory-data.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
+//  angular-in-memory-web-api
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisteredlistComponent } from './registeredlist/registeredlist.component';
@@ -19,15 +24,27 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
 
     BrowserModule,
-    FormsModule
+    FormsModule,
+
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+// HttpClientInMemoryWebApiModule.forRoot(
+//   InMemoryDataService, { dataEncapsulation: false }
+// )
