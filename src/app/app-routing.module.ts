f7752f0f9e42d582f1c5d82055fcb7a091cee043
syntax3rror <winzter@gmail.com>
2019-05-31 13:36:48 +0800

    Move registration form to regisration component    modified:   src/app/app-routing.module.ts    modified:   src/app/app.component.html    copied:     src/app/app.component.html -> src/app/registration/registration.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { HeroesComponent }      from './heroes/heroes.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
+import { RegistrationComponent }  from './registration/registration.component';
+
 //const routes: Routes = [];
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Setting as Default route
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Setting as Default route
   { path: 'detail/:id', component: HeroDetailComponent }, // Adding the Details to new route
+
+  { path: 'registration', component: RegistrationComponent },
+
 ];
 
 @NgModule({
