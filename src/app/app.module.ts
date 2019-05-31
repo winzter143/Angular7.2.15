d08ee43d26deb833d769de072455950978d3be05
syntax3rror <winzter@gmail.com>
2019-05-31 13:03:26 +0800

    Create new component registration    modified:   src/app/app.module.ts    new file:   src/app/registration/registration.component.css    new file:   src/app/registration/registration.component.html    new file:   src/app/registration/registration.component.spec.ts    new file:   src/app/registration/registration.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule }    from '@angular/common/http';
 
 //  angular-in-memory-web-api
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 import { RegisteredlistComponent } from './registeredlist/registeredlist.component';
+
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    RegistrationComponent
 
   ],
   imports: [
@@ -34,17 +38,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     FormsModule,
 
     HttpClientModule,
-    
+    //HttpClientInMemoryWebApiModule.forRoot(InMemHeroService),
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-// and returns simulated server responses.
-// Remove it when a real server is ready to receive requests.
-// HttpClientInMemoryWebApiModule.forRoot(
-//   InMemoryDataService, { dataEncapsulation: false }
-// )
