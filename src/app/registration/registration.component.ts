f15a67f93b7a301c7bed618178a9a76a1a465f63
syntax3rror <winzter@gmail.com>
2019-06-04 10:59:47 +0800

    Adding implementation on user form and modal    modified:   src/app/registration/registration.component.html    modified:   src/app/registration/registration.component.ts    new file:   src/app/registration/user.ts

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+import { User } from './user';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  @Input() user: User;
+
   constructor() { }
 
   ngOnInit() {
+    console.log("RegistrationComponent onInit");
+    this.user = {
+      id: 1,
+      email: "winzter@gmail.com",
+      password: 'password',
+      firstname: 'jonh',
+      lastname: 'doe',
+      street: 'heloo', // Apartment, studio, or floor
+      city: 'city',
+      province: 'manila',
+      zipcode: '1220'
+    }
+  }
+
+  register(): void {
+      console.log('Register');
   }
 
 }
---
+++ src/app/registration/user.ts
@@ -0,0 +1,12 @@
+export class User {
+  id: number;
+  email: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  street: string; // Apartment, studio, or floor
+  city: string;
+  province: string;
+  zipcode: string;
+
+}
